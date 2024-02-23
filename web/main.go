0e28f0fcc822b71bef610bd89256012074387f3d
Nils Kempen <nils.kempen@jn2p.de>
2024-02-23 21:39:02 +0100

    its works, all your language are belong to us

--- web/main.go
+++ web/main.go
@@ -6,21 +6,29 @@ import (
     "html/template"
 )
 
+
 var rootTmpl *template.Template
+
+type Words struct{
+    Results []string
+}
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("got / request\n")
+    rootTmpl = template.Must(template.ParseFiles("html/index.html"))
     rootTmpl.Execute(w, r.UserAgent())
 }
 
 func query(w http.ResponseWriter, r *http.Request) {
-    log.Printf(r.PostFormValue("query"))
+    rootTmpl = template.Must(template.ParseFiles("html/index.html"))
+    testData := []string{"Hallo", "WElt", "lorem", "ipsum" }
+    res := Words{Results: testData}
+    rootTmpl.ExecuteTemplate(w, "word-list-el", res)
 }
 
 func main() {
     http.HandleFunc("/", getRoot)
     http.HandleFunc("/query", query)
-    rootTmpl = template.Must(template.ParseFiles("html/index.html"))
 
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
