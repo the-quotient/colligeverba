2d32ff995958f57f0b4074ff46169f0c60a5a64a
thequotient <thequotient@jn2p.de>
2024-02-23 20:40:14 +0100

    Two question marks now correspond exactly two arbitrary characters

--- engine/regex.go
+++ engine/regex.go
@@ -10,7 +10,7 @@ func TransformToRegEx(pattern string) *regexp.Regexp {
 	step1 := strings.Replace(pattern, "?{}", ".", -1)
 	step2 := strings.Replace(step1, "?{", "[", -1)
 	step3 := strings.Replace(step2, "}", "]", -1)
-	step4 := strings.Replace(step3, "??", ".+", -1)
+	step4 := strings.Replace(step3, "??", ".{1,2}", -1)
 
 	return regexp.MustCompile(step4)
 }
