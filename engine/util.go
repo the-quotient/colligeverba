ccaf5052918683ae6a94f71682426980d02227af
thequotient <thequotient@jn2p.de>
2024-02-23 16:52:57 +0100

    Pattern with multiple candidates to regex

--- engine/util.go
+++ engine/util.go
@@ -1,5 +1,9 @@
 package engine
 
+import (
+	"container/list"
+)
+
 func ReplaceChar(s string, oldChar byte, newChar byte) string {
 	strBytes := []byte(s)
 
@@ -10,4 +14,25 @@ func ReplaceChar(s string, oldChar byte, newChar byte) string {
 	}
 
 	return string(strBytes)
+}
+
+func StringToList(s string) *list.List {
+	l := list.New()
+
+	for _, char := range s {
+		l.PushBack(char)
+	}
+
+	return l
+}
+
+func insertAtPosition(l *list.List, value interface{}, position int) *list.Element {
+	var i int
+	for e := l.Front(); e != nil; e = e.Next() {
+		if i == position {
+			return l.InsertBefore(value, e)
+		}
+		i++
+	}
+	return l.PushBack(value)
 }
