660195a5f716b00bac22537b74f77aa6bc5e73dc
thequotient <thequotient@jn2p.de>
2024-02-23 18:01:53 +0100

    TransformToRefEx now returns an already compiled regex

--- engine/regex.go
+++ engine/regex.go
@@ -1,16 +1,16 @@
 package engine
 
 import (
+	"regexp"
 	"strings"
 )
 
-func TransformToRegEx(pattern string) string {
+func TransformToRegEx(pattern string) *regexp.Regexp {
 
 	step1 := strings.Replace(pattern, "?{}", ".", -1)
 	step2 := strings.Replace(step1, "?{", "[", -1)
 	step3 := strings.Replace(step2, "}", "]", -1)
 	step4 := strings.Replace(step3, "??", ".+", -1)
 
-	return step4
-
+	return regexp.MustCompile(step4)
 }
