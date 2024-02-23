37b0e48b4c0b3013316e740f482c7f1d35cb5b65
thequotient <thequotient@jn2p.de>
2024-02-23 16:54:11 +0100

    Added more tests

--- engine-test/main.go
+++ engine-test/main.go
@@ -9,7 +9,11 @@ import (
 func main() {
 
 	// this is given by the web container to the engine
-	pattern := "??c?esi??"
+	pattern1 := "?{}cclesia"
+	pattern2 := "?{}c?{}?{}"
+	pattern3 := "?{}?{}co"
+	pattern4 := "d?{l,k,j,i}c?{a,b,h,o}"
+	pattern5 := "?{}?{}c?{l,m,n}esi?{}?{}"
 
 	latinBasicForms := [...]string{
 		"dicere",
@@ -33,13 +37,63 @@ func main() {
 		"ecclesias",
 		"ecclesiis"}
 
-	word1 := engine.SearchForWord(pattern, latinBasicForms[:])
-	word2 := engine.SearchForWord(pattern, latinInflectedForms[:])
+	fmt.Println("Test 1: pattern = " + pattern1)
+	word11 := engine.SearchForWord(pattern1, latinBasicForms[:])
+	word12 := engine.SearchForWord(pattern1, latinInflectedForms[:])
 
-	for e := word1.Front(); e != nil; e = e.Next() {
+	for e := word11.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
-	for e := word2.Front(); e != nil; e = e.Next() {
+	for e := word12.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
+	fmt.Println()
+
+	fmt.Println("Test 1: pattern = " + pattern2)
+	word21 := engine.SearchForWord(pattern2, latinBasicForms[:])
+	word22 := engine.SearchForWord(pattern2, latinInflectedForms[:])
+
+	for e := word21.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	for e := word22.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	fmt.Println()
+
+	fmt.Println("Test 1: pattern = " + pattern3)
+	word31 := engine.SearchForWord(pattern3, latinBasicForms[:])
+	word32 := engine.SearchForWord(pattern3, latinInflectedForms[:])
+
+	for e := word31.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	for e := word32.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	fmt.Println()
+
+	fmt.Println("Test 1: pattern = " + pattern4)
+	word41 := engine.SearchForWord(pattern4, latinBasicForms[:])
+	word42 := engine.SearchForWord(pattern4, latinInflectedForms[:])
+
+	for e := word41.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	for e := word42.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	fmt.Println()
+
+	fmt.Println("Test 1: pattern = " + pattern5)
+	word51 := engine.SearchForWord(pattern5, latinBasicForms[:])
+	word52 := engine.SearchForWord(pattern5, latinInflectedForms[:])
+
+	for e := word51.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	for e := word52.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	fmt.Println()
 }
