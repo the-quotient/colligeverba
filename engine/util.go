940bd806f4282d19ab3e97b99cbdce0b7104b13b
thequotient <thequotient@jn2p.de>
2024-02-24 02:38:57 +0100

    Cleaned util

--- engine/util.go
+++ engine/util.go
@@ -5,18 +5,6 @@ import (
 	"fmt"
 )
 
-func ReplaceChar(s string, oldChar byte, newChar byte) string {
-	strBytes := []byte(s)
-
-	for i := range strBytes {
-		if strBytes[i] == oldChar {
-			strBytes[i] = newChar
-		}
-	}
-
-	return string(strBytes)
-}
-
 func StringToList(s string) *list.List {
 	l := list.New()
 
@@ -28,16 +16,13 @@ func StringToList(s string) *list.List {
 }
 
 func TransformToArray(l *list.List) []string {
-	// Initialize a slice with capacity equal to the length of the list
+
 	slice := make([]string, 0, l.Len())
 
-	// Iterate over the list and append each element to the slice
 	for e := l.Front(); e != nil; e = e.Next() {
-		// Assuming the list contains string values, assert the type to string
 		if str, ok := e.Value.(string); ok {
 			slice = append(slice, str)
 		} else {
-			// Handle the case where the type assertion fails
 			fmt.Println("Warning: Non-string value encountered in the list, skipping.")
 		}
 	}
