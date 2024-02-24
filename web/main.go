eed0d5641d85f04a38b13bcc6f759711e366bcdf
Nils Kempen <nils.kempen@jn2p.de>
2024-02-24 01:38:33 +0100

    embedd templates

--- web/main.go
+++ web/main.go
@@ -1,39 +1,45 @@
 package main
 
 import (
+	"embed"
+	"github.com/the-quotient/vestigiaverbi/engine"
+	"html/template"
 	"log"
 	"net/http"
-    "html/template"
-    "github.com/the-quotient/vestigiaverbi/engine"
 )
 
+var (
+	//go:embed html/*
+	content embed.FS
+)
 
 var rootTmpl *template.Template
 
-type Words struct{
-    Results []string
+type Words struct {
+	Results []string
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("got / request\n")
-    rootTmpl = template.Must(template.ParseFiles("html/index.html"))
-    rootTmpl.Execute(w, r.UserAgent())
+    rootTmpl = template.Must(template.ParseFS(content, "html/index.html"))
+	//rootTmpl = template.Must(template.ParseFiles("html/index.html"))
+	rootTmpl.Execute(w, r.UserAgent())
 }
 
 func query(w http.ResponseWriter, r *http.Request) {
-    rootTmpl = template.Must(template.ParseFiles("html/index.html"))
-    query := r.FormValue("query")
-    res := engine.Search(query)
-    if len(res) > 10 {
-        res = res[:10]
-    }
-    rootTmpl.ExecuteTemplate(w, "word-list-el", Words{Results: res})
+	//rootTmpl = template.Must(template.ParseFiles("html/index.html"))
+    rootTmpl = template.Must(template.ParseFS(content, "html/index.html"))
+	query := r.FormValue("query")
+	res := engine.Search(query)
+	if len(res) > 10 {
+		res = res[:10]
+	}
+	rootTmpl.ExecuteTemplate(w, "word-list-el", Words{Results: res})
 }
 
 func main() {
-    http.HandleFunc("/", getRoot)
-    http.HandleFunc("/query", query)
-
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/query", query)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
