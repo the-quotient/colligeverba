c9f90b93a845336316a57f24e699047fbd555ad8
thequotient <thequotient@jn2p.de>
2025-12-31 13:30:06 +0100

    Add toggle for database

--- web/main.go
+++ web/main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/the-quotient/colligeverba/db"
@@ -24,6 +25,7 @@ type Words struct {
 }
 
 var gdb *sql.DB
+var useDB bool
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	rootTmpl = template.Must(template.ParseFS(content, "html/index.html"))
@@ -44,45 +46,63 @@ func query(w http.ResponseWriter, r *http.Request) {
 func wordInfo(w http.ResponseWriter, r *http.Request) {
 	infoTmpl := template.Must(template.ParseFS(content, "html/modal.html"))
 	word := r.PathValue("word")
-    winfo := getWordInfo(word)
+	winfo := getWordInfo(word)
 	infoTmpl.Execute(w, winfo)
 }
 
 func getWordInfo(word string) db.WordInfo {
 	var winfo db.WordInfo
+
 	if word != "Invalid input" && word != "No matches found" {
-		winfo, err := db.GetItemByWord(gdb, word)
-		if err != nil {
-			log.Printf("Cannot find '%s' in db, trying online \n", word)
-			bf, m, fa := engine.GetInformation(word)
-			winfo = db.WordInfo{
-				Word:         word,
-				BasicForm:    bf,
-				Meanings:     m,
-				FormAnalysis: fa,
+		
+		// Try DB first (if enabled)
+		if useDB {
+			foundInfo, err := db.GetItemByWord(gdb, word)
+			if err == nil {
+				log.Printf("Returning WordInfo for '%s' from Cache! \n", word)
+				return foundInfo
 			}
-            err = db.InsertItem(gdb, winfo)
-            if err != nil {
-                log.Println("Failed to insert in DB")
-                log.Println(err)
-            }
-            return winfo
-		} else {
-            log.Printf("Returning WordInfo for '%s' from Cache! \n", word)
-            return winfo
-        }
+		}
+
+		// Not in DB (or DB disabled) -> Fetch Online
+		log.Printf("Cannot find '%s' in db (or db disabled), trying online \n", word)
+		bf, m, fa := engine.GetInformation(word)
+		winfo = db.WordInfo{
+			Word:         word,
+			BasicForm:    bf,
+			Meanings:     m,
+			FormAnalysis: fa,
+		}
+
+		// Save to DB (if enabled)
+		if useDB {
+			err := db.InsertItem(gdb, winfo)
+			if err != nil {
+				log.Println("Failed to insert in DB")
+				log.Println(err)
+			}
+		}
+		return winfo
 	}
-	//try to find in db
 	return winfo
 }
 
 func main() {
+	// Check Environment Variable to toggle Database
+	if os.Getenv("DISABLE_DB") == "true" {
+		useDB = false
+		log.Println("Running in Stateless Mode (Database Disabled)")
+	} else {
+		useDB = true
+		log.Println("Running in Stateful Mode (Database Enabled)")
+		gdb = db.InitDb()
+		defer db.CloseDb(gdb)
+	}
+
 	http.HandleFunc("POST /wordinfo/{word}", wordInfo)
 	http.HandleFunc("/query", query)
 	http.HandleFunc("/", getRoot)
 
-	gdb = db.InitDb()
-    defer db.CloseDb(gdb)
-
+	log.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
